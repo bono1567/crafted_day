38d32381a3477d86d603fbaddf71f8b8d8b5d04c
bono1567 <bono10200807@hotmail.com>
2019-12-25 00:39:11 +0530

    Weaver Update
    Signed-off-by: bono1567 <bono10200807@hotmail.com>

--- Harvester/HeadlineHarvester/Weaver.py
+++ Harvester/HeadlineHarvester/Weaver.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from Harvester.HeadlineHarvester.DataArrangement import FTArrangeWithWords
-from Harvester.StockPriceHarvester.DataArrangement import ArrangedData
+from Harvester.StockPriceHarvester.DataRetriever import AlphaVantageStocks
 
 """DataWeaver will return the headline with the date and trend with the respective headline"""
 
@@ -9,11 +9,25 @@ from Harvester.StockPriceHarvester.DataArrangement import ArrangedData
 class DataWeaver:
     def __init__(self, words, stock):
         self.model_headlines = FTArrangeWithWords(words)
-        self.model_stocks = ArrangedData('D')
+        self.model_stocks = AlphaVantageStocks(stock)
         self.sym = [stock]
 
+    @staticmethod
+    def __trend_generator(data):
+        trend = []
+        for difference in data.close.diff():
+            if difference > 0:
+                trend.append('UP')
+            elif difference < 0:
+                trend.append('DOWN')
+            else:
+                trend.append('NC')
+
+        return trend
+
     def __get_the_data__(self):
-        data = self.model_stocks.fetch(self.sym, 365.25 * 4)[0]
+        data = self.model_stocks.fetch(365.25 * 4, 'D')
+        data['trend'] = self.__trend_generator(data)
         return data
 
     def __weave__(self):
@@ -32,8 +46,8 @@ class DataWeaver:
         return pd.merge(self.model_headlines.getSummary(title), self.__weave__(), on='time')
 
 
-""" if __name__=='__main__':
+"""if __name__ == '__main__':
     A = DataWeaver(['India', 'Modi', 'Cars'], 'NSE:TATAMOTORS')
-    data = A.fetch(False)
+    data = A.fetch(True)
 
-    print(data.head().shape)"""
+    print(np.unique(data['trend']))"""
--- Harvester/LocalDataSave.py
+++ Harvester/LocalDataSave.py
@@ -1,18 +1,19 @@
 from Harvester.StockPriceHarvester.DataArrangement import ArrangedData
 from Harvester.HeadlineHarvester.Weaver import DataWeaver
+stocks_to_analyse = ['ETR:VOW3', 'NSE:BRITANNIA', 'NSE:TATAMOTORS']
+headline_words = [['volkswagen', 'cars', 'petrol', 'diesel', 'transport', 'vehicles', 'german', 'economy'],
+                  ['britannia', 'food', 'India', 'elections', 'Bombay', 'stocks'],
+                  ['vehicles', 'Tata', 'America', 'petrol', 'diesel', 'India']]
 
-stocks_to_analyse = ['NSE:TATAMOTORS']
-headline_words = [['vehicle', 'Tata', 'America', 'petrol', 'diesel', 'India']]
-
-# ['volkswagen', 'cars', 'petrol', 'diesel', 'transport', 'vehicles', 'german', 'economy'],
-                 # ['britannia', 'food', 'India', 'elections', 'Bombay', 'stocks'] ['ETR:VOW3', 'NSE:BRITANNIA',
 A = ArrangedData("D")
 
-# data = A.fetch(stocks_to_analyse, 1461)
-# print(data[0].head())
-for name, words_set in zip(stocks_to_analyse, headline_words):
-    # x.to_csv('./resources/' + name + '.csv')
+data = A.fetch(stocks_to_analyse, 1461)
+print(data[0].head())
+for name, x, words_set in zip(stocks_to_analyse, data, headline_words):
+    x.to_csv('./resources/' + name + '.csv')
     B = DataWeaver(words_set, name)
     data = B.fetch(True)
     data.to_csv('./resources/' + name + '_NEWS' + '.csv')
+
+
 
